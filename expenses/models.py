670f8ab0b5a04e1b419a76a373db47159e1777eb
mustafaoguzbaran <mustafaoguzbaran@icloud.com>
2024-12-09 13:54:42 +0300

    SPEN-15 Feature: in order to increase the accuracy of savings suggestions, different weight definitions were defined according to expenditure categories.

--- expenses/models.py
+++ expenses/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
+    weight = models.FloatField(default=1.0)
 
     def __str__(self):
         return self.name
